a0922bab4d1c8e798bba0c7eec533588a0b49f75
Stevee67 <stopa6767@gmail.com>
2016-06-02 03:32:50 +0300

    m

--- config.py
+++ config.py
@@ -12,11 +12,12 @@ class Config:
         DB_USER = os.environ.get('OPENSHIFT_POSTGRESQL_DB_USERNAME')
         DB_HOST = os.environ.get('OPENSHIFT_POSTGRESQL_DB_HOST')
         DB_PASS = os.environ.get('OPENSHIFT_POSTGRESQL_DB_PASSWORD')
-        DB_PORT = os.environ.get('OPENSHIFT_POSTGRESQL_DB_PORT')
+        DB_PORT = int(os.environ.get('OPENSHIFT_POSTGRESQL_DB_PORT'))
     SITE_TITLE = ''
     DATABASE_URI = \
         database_uri(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT)
-    PORT = os.environ.get('OPENSHIFT_PYTHON_PORT') or 8888
+
+    PORT = int(os.environ.get('OPENSHIFT_PYTHON_PORT')) if os.environ.get('OPENSHIFT_PYTHON_PORT') else 8888
     HOST = os.environ.get('OPENSHIFT_PYTHON_IP') or '0.0.0.0'
     DEBUG = True
     SECRET_KEY = secret_data.SECRET_KEY
